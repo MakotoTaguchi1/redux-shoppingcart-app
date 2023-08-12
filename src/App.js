25ff626b7207e83788f90717fbd37d2c062b58f5
MakotoTaguchi1 <makoto.taguchi1@x-factory.jp>
2023-08-12 10:31:43 +0900

    feat: 合計計算のreducer作成, useEffect利用

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
 import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals } from './features/cart/CartSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />

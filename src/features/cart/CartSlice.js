86bd64d180f1b7d71d6a91227d65a96264b98ae9
MakotoTaguchi1 <makoto.taguchi1@x-factory.jp>
2023-08-12 10:18:55 +0900

    feat: 商品数増減のreducer作成

--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -21,10 +21,24 @@ const cartSlice = createSlice({
       // クリックした商品ID以外を残す（filter）
       state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
     },
+    increase: (state, action) => {
+      // クリックした商品のIDを抽出
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, action) => {
+      // クリックした商品のIDを抽出
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 
 // console.log(cartSlice);
 
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 export default cartSlice.reducer;

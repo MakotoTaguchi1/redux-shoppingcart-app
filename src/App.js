4a7fff824d4269179c62c528a00fe04c47b7f05d
MakotoTaguchi1 <makoto.taguchi1@x-factory.jp>
2023-08-12 11:01:34 +0900

    feat: 全削除時にモーダル開くようにする

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
 import { useDispatch, useSelector } from 'react-redux';
 import { calculateTotals } from './features/cart/CartSlice';
+import Modal from './components/Modal';
 
 function App() {
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
 
   return (
     <main>
+      {isOpen && <Modal />}
+
       <Navbar />
       <CartContainer />
     </main>
